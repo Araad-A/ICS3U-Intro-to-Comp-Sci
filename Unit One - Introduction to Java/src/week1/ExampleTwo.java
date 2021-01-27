a498f2a1ff4635db6b9891f4790226ed45e5f8a0
Araad Alguneh <aalguneh@bayviewglen.ca>
2021-01-27 10:58:41 -0500

    Day 2 Homework (Print)

---
+++ Unit One - Introduction to Java/src/week1/ExampleTwo.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("\tA\t\tA\n      A  A\t      A  A\n     A    A\t     A    A\n    AAAAAAAA\t    AAAAAAAA\n   A        A\t   A        A \n  A          A\t  A          A\n A            A\t A            A");
+    //Code above codes my initials AA with the letter A
+    //Below will code picture (House)
+
+        System.out.println("\tA\n      A  A\n     A    A\n    A      A\n   A        A\n  A          A\n AAAAAAAAAAAAAA\n A\t      A\n A\t      A\n A\t      A\n A\t      A\n AAAAAAAAAAAAAA");
+    //Code below will print out the Bayview Glen mailing address
+
+        System.out.println("\n 85 Moatfield Dr, North York, ON M3B 3L6");
+
+    //Code below will print the Lady Mercy Hospital billing statements
+        System.out.println("\n/////////////////////////////////////////////////////\n/\t\t\t\t\t\t    /\n/\t     Our Lady of Mercy Hospital\t\t    /\n/\t     --------------------------\t\t    /\n/\t\t\t\t\t\t    /\n/\t\t 1306 Central City\t\t    /\n/\t       Phone (416) 555-1212\t\t    /\n/\t\t\t\t\t\t    /\n/////////////////////////////////////////////////////");
+
+    }
+}
