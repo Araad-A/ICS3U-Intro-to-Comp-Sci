ecf83c2468415372768ecab7302e2de539854cd4
Araad Alguneh <aalguneh@bayviewglen.ca>
2021-01-29 22:08:59 -0500

    Day 3 Class work / examples

---
+++ Unit One - Introduction to Java/src/week1/StringConcatanation.java
@@ -0,0 +1,17 @@
+package week1;
+
+public class StringConcatanation {
+    public static void main(String[] args) {
+        exampleOne();
+    }
+
+    private static void exampleOne() {
+    int markOne = 90;
+    int markTwo = 94;
+    int markThree = 88;
+    
+    double average = (markOne + markTwo + markThree) / 3.0;
+    
+    System.out.println(average);
+    }
+}
---
+++ Unit One - Introduction to Java/src/week1/VariablesAndAssignmentOperator.java
@@ -0,0 +1,16 @@
+package week1;
+
+public class VariablesAndAssignmentOperator {
+    /**
+     * InnerVariablesAndAssignmentOperator
+     */
+    public static void main(String[] args) {
+        exampleOne();
+    }
+        private static void exampleOne () {
+            int numberOne;
+
+            numberOne = 10;
+            System.out.println(numberOne);
+        }
+}
